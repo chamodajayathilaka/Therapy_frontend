739da1dc18c76404a592dd4cc94e616ffac20da4
chamoda <chamodajayathilaka96@mail.com>
2023-10-01 20:09:45 +0530

    second commit

---
+++ src/components/Footer.jsx
@@ -0,0 +1,16 @@
+//import Container from 'react-bootstrap/Container';
+import Navbar from 'react-bootstrap/Navbar';
+
+function Footer() {
+  return (
+    <>
+      
+      
+      <Navbar bg="dark" height="50px">
+        abc
+      </Navbar>
+    </>
+  );
+}
+
+export default Footer;
